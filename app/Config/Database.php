74a9bbf55fd1dfd7e1347d67983d088c28fa6c7e
satria71 <satriabumiputralangit@gmail.com>
2025-08-27 15:20:09 +0700

    setting databases

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => 'ICS',
+        'username'     => 'satria',
+        'password'     => '123',
+        'database'     => 'ics',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
