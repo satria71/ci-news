91c5489ad6eec13e4013f9ce569f89eb53d1a8c8
satria71 <satriabumiputralangit@gmail.com>
2025-11-01 13:52:02 +0700

    modif dashboard

--- app/Views/home.php
+++ app/Views/home.php
@@ -11,7 +11,7 @@
         </div>
 
         <div class="section-body">  
-            <div class="row">
+            <!-- <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
                         <div class="card-icon bg-success">
@@ -72,7 +72,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
             <div class="row">
               <div class="col-md-8">
